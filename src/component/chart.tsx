2a9856616a2ef7d6700b4e4ae7e4d989181db5f3
fishshi <2855691008@qq.com>
2025-06-25 15:13:23 +0800

    opti chart type

--- src/component/chart.tsx
+++ src/component/chart.tsx
@@ -16,16 +16,19 @@ function Chart({
 
   useEffect(() => {
     // 模拟每年资产计算
-    const leftBalance: number[] = [balance];
+    const data: [number, number][] = [[0, balance]];
     let currentBalance = balance;
     let currentExpense = expense;
 
-    while (currentBalance > 0 && leftBalance.length < 1000) {
+    while (currentBalance > 0 && data.length < 100) {
       currentExpense *= 1 + inflationRate;
       currentBalance -= currentExpense;
       currentBalance *= 1 + investmentReturnRate;
-      if (currentBalance < 0) currentBalance = 0;
-      leftBalance.push(currentBalance);
+      if (currentBalance >= 0) {
+        data.push([data.length, currentBalance]);
+      } else {
+        // To do
+      }
     }
 
     if (!chartRef.current) return;
@@ -33,16 +36,14 @@ function Chart({
 
     chart.setOption({
       xAxis: {
-        type: "category",
-        data: leftBalance.map((_, i) => i), // 年数
+        name: "年数",
       },
       yAxis: {
-        type: "value",
         name: "剩余资产",
       },
       series: [
         {
-          data: leftBalance,
+          data: data,
           type: "line",
           smooth: true,
         },
@@ -54,7 +55,11 @@ function Chart({
     };
   }, [balance, expense, inflationRate, investmentReturnRate]);
 
-  return <div ref={chartRef} style={{ width: "100%", height: "500px" }} />;
+  return (
+    <>
+      <div ref={chartRef} style={{ width: "100%", height: "500px" }} />;
+    </>
+  );
 }
 
 export default Chart;
